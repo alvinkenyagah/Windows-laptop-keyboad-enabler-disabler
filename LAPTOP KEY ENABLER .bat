8d1b0aa0cef0adf3aaba9a3b0047c72718bedfde
alvinkenyagah <82902370+alvinkenyagah@users.noreply.github.com>
2023-01-16 19:40:42 +0300

    Initial files

---
+++ LAPTOP KEY ENABLER .bat
@@ -0,0 +1,21 @@
+@echo off
+if _%1_==_payload_  goto :payload
+
+:getadmin
+    echo %~nx0: elevating self
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+
+:payload
+
+@ECHO KEYBOARD ENABLER
+pause
+
+sc config i8042prt start= auto
+ECHO Your PC will REBOOT please be patient...
+timeout /t 5 /nobreak
+shutdown.exe /r /t 00
